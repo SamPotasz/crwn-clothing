746d3baa79818ca84051be56376e8ab3ca5ee2f1
SamPotasz <sampotasznik@gmail.com>
2020-03-27 15:24:38 -0500

    Styled homepaged. Added initial menu components.

---
+++ src/pages/hompepage/hompage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+import DirectoryMenu from '../../directory-menu/directory-menu.component';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <DirectoryMenu />
+    </div>
+)
+
+export default HomePage;
