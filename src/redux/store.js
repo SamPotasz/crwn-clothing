d044abbb064e75a90456fa90d8f214e6e693992c
SamPotasz <sampotasznik@gmail.com>
2020-04-06 11:34:29 -0500

    Added redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';  //allows browser to cache the store
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
@@ -6,6 +7,8 @@ import rootReducer from './root-reducer';
 const middleWares = [logger];
 
 //instantiate the redux store!
-const store = createStore(rootReducer, applyMiddleware(...middleWares));
+export const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
